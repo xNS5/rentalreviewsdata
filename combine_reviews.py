3fd196dc22ddedaa34087bfadadfda24a771ba0d
Michael Kennedy <michael@michaelkennedy.dev>
2024-09-09 13:55:15 -0700

    Adding cleanup function + call to new create_directory utility function to delete unneeded google and yelp files

--- combine_reviews.py
+++ combine_reviews.py
@@ -5,9 +5,9 @@ import utilities
 def getPrefix(input, delimiter):
      return input.split(delimiter)[0]
 
-output_path = "./reviews/"
-google_path = "./google_input/output/"
-yelp_path = "./yelp_input/output/"
+output_path = "merged_reviews"
+google_path = "google"
+yelp_path = "yelp"
 
 
 def merge(file1, file2):
@@ -35,7 +35,6 @@ def write(obj, inputPath, outputPath):
 def filter(input_path, alt_path):
     input_list = utilities.list_files(input_path)
     input_prefix = "google" if "google" in input_path else "yelp"
-    # alt_prefix = "yelp" if "yelp" in alt_path else "google"s
     
     company_map = utilities.company_map(input_prefix)
 
@@ -57,7 +56,14 @@ def filter(input_path, alt_path):
         else:
             file_json = merge(f"{input_path}{file}", f"{alt_path}{file}")
             write(file_json, None, f"{output_path}{file}")
-    
+
+def cleanup(path):
+    utilities.remove_path(path)
+
+utilities.create_directory("merged_reviews")
 
 filter(yelp_path, google_path)
 filter(google_path, yelp_path)
+
+cleanup(yelp_path)
+cleanup(google_path)

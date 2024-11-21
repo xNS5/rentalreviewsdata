4491f7dcb58160871622ce2687cf8e7b10140847
Michael Kennedy <michael@michaelkennedy.dev>
2024-11-20 16:48:13 -0800

    Removing tests, too annoying to implement as-is

--- seed.py
+++ seed.py
@@ -13,18 +13,23 @@ config = {**dotenv_values(".env")}
 
 seed_config = get_seed_config()
 
+verbose = False
+
+
 def get_params():
     database_selection = None
     database_action = None
     database_environment = None
     database_options = ["mongodb", "firebase"]
     action_options = ["seed", "clear", "list", "update", "re-seed"]
-    env_options = ["test", "prod"]
+    env_options = ["test", "production"]
+    confirmation_options = ["Yes", "No"]
     if len(sys.argv) > 1:
         parser = argparse.ArgumentParser()
         parser.add_argument("-db", "--database", required=True, help="Database Name")
         parser.add_argument("-a", "--action", required=True, help="Action to perform on the database")
-        parser.add_argument("-env", "--env", required=False, help="Database Environment [test|prod]`")
+        parser.add_argument("-env", "--env", type=str, required=False, help="Database Environment [test|production]`")
+        parser.add_argument("-verbose", "--verbose", required=False, help="Verbose output", action="store_true")
 
         args = parser.parse_args()
 
@@ -40,13 +45,20 @@ def get_params():
         else:
             database_action = args.action.lower()
 
-        if args.env is not None:
-            if args.env.lower() not in env_options:
-                print("Invalid database environment")
-                exit(1)
+        if args.database.lower() == "firebase":
+            if args.env is not None:
+                if args.env.lower() not in env_options:
+                    print("Invalid database environment")
+                    exit(1)
+                else:
+                    database_environment = args.env.lower()
             else:
-                database_environment = args.env.lower()
+                print("Environment required for Firebase")
+                exit(1)
 
+        if args.verbose is not None:
+            global verbose
+            verbose = True
 
     else:
         database_environment = None
@@ -62,6 +74,13 @@ def get_params():
             database_environment = pyinput.inputMenu(
                 env_options, lettered=True, numbered=False
             ).lower()
+
+    if database_environment == "production":
+        confirmation = pyinput.inputMenu(
+                confirmation_options, lettered=True, numbered=False, prompt="You selected the \"production\" option -- this will affect the production database. Are you sure you wish to continue?\r\n"
+            ).lower()
+        if confirmation == "no":
+            exit(1)
     return database_selection, database_action, database_environment
 
 def list_collections(db, client):
@@ -100,10 +119,11 @@ def squash_file(db, client, path_obj):
                 db[squash_config['collection']].insert_one(seed_obj) 
             case "firebase":
                 db.collection(squash_config["collection"]).document(squash_config['document_name']).set(seed_obj)                    
-    
-        print(f'Squashed \033[1m{path_obj["path"]}\033[0m in {client}')
+
+        if verbose:
+            print(f'Squashed \033[1m{path_obj["path"]}\033[0m in {client}')
     except Exception as e:
-        print(f"Failed to squash on {client} with error: {traceback.print_exc()} {e}")
+        print(f"Failed to squash on {client} with error: {traceback.print_exc()} {e}", file=sys.stderr)
         return
 
 def create_index(db, client, path_obj):
@@ -132,12 +152,11 @@ def create_index(db, client, path_obj):
     
         print(f'Created index on {client} for {path_obj["path"]}')
     except Exception as e:
-        print(f"Failed to seed {client} with error: {traceback.print_exc()} {e}")
+        print(f"Failed to seed {client} with error: {traceback.print_exc()} {e}", file=sys.stderr)
         return
    
 
 def populate(db, client, path_obj):
-
     seed_arr = []
     input_path = path_obj["path"]
     files = list_files(input_path)
@@ -174,9 +193,10 @@ def populate(db, client, path_obj):
                         batch.set(doc_ref, temp_obj)
                 batch.commit()
     except Exception as e:
-        print(f"Failed to seed {client} with error: {traceback.print_exc()} {e}")
+        print(f"Failed to seed {client} with error: {traceback.print_exc()} {e}", file=sys.stderr)
         return
-    print(f'Seeded {client} with {len(seed_arr)} records from {path_obj["path"]}.')
+    if verbose:
+        print(f'Seeded {client} with {len(seed_arr)} records from {path_obj["path"]}.')
 
 
 def clear_db(db, client):
@@ -197,10 +217,10 @@ def clear_db(db, client):
                         batch.delete(doc_ref)
                 batch.commit()
     except Exception as e:
-        print(f"Failed to clear {client} with error {e}")
+        print(f"Failed to clear {client} with error {e}", file=sys.stderr)
         return
-
-    print(f"Cleared {client}")
+    if verbose:
+        print(f"Cleared {client}")
 
 
 def update(db, client, path_obj):
@@ -244,10 +264,9 @@ def update(db, client, path_obj):
         print(f"No updates available in {update_path}")
            
 
-def main():
-    db_name, db_action, db_env = get_params()
-    db = None
+def main(db_name, db_action, db_env = None):
     try:
+        db = None
         match db_name:
             case "mongodb":
                 import pymongo
@@ -270,31 +289,38 @@ def main():
                 cred = credentials.Certificate(certificate_path)
                 app = firebase_admin.initialize_app(cred)
                 db = firestore.client(app)
-               
-        print(f"Initialized connection to {db_name}")
+
+        if verbose:
+            print(f"Initialized connection to {db_name}")
+
+        def exec_action(action):
+            for path in seed_config:
+                if path['squash']:
+                    squash_file(db, db_name, path)
+                else:
+                    match action:
+                        case "seed" | "re-seed":
+                            populate(db, db_name, path)
+                        case "update":
+                            update(db, db_name, path)
+
+        actions = {
+            "clear": lambda: clear_db(db, db_name),
+            "re-seed": lambda: (clear_db(db, db_name), exec_action(db_action)),
+            'update': lambda: exec_action(db_action),
+            'seed': lambda: exec_action(db_action),
+            'list': lambda: print(list_collections(db, db_name), file=sys.stdout)
+        }
+
+        if db_action in actions:
+            actions[db_action]()
     except Exception as e:
-        print(f"Failed to initialize source {db_name} with exception:\n{e}")
+        print(f"Failed to initialize source {db_name} with exception:\n{e}", file=sys.stderr)
         return
-    
-    def exec(action):
-        for path in seed_config:
-            if path['squash']:
-                squash_file(db, db_name, path)
-            else:
-                match action:
-                    case "seed" | "re-seed":
-                        populate(db, db_name, path)
-                    case "update":
-                        update(db, db_name, path)
 
-    actions = {
-        "clear": lambda: clear_db(db, db_name),
-        "re-seed": lambda: (clear_db(db, db_name), exec(db_action)),
-        'update': lambda: exec(db_action),
-        'seed': lambda: exec(db_action),
-        'list': lambda: print(list_collections(db, db_name))
-    }
 
-    if db_action in actions:
-        actions[db_action]()
 
+
+if __name__ == "__main__":
+    db_name, db_action, db_env = get_params()
+    # main(db_name, db_action, db_env)
